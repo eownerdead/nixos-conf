195a7a95c286ab99abed955dcc840f6e73eabc8e
EOWNERDEAD <eownerdead@disroot.org>
2022-03-05 22:54:50 +0900

    Add my blueprint-compiler

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,4 +2,5 @@
 with pkgs; {
   gittyup = (callPackage ./gittyup/default.nix { });
   adw-gtk3 = (callPackage ./adw-gtk3/default.nix { });
+  blueprint-compiler = (callPackage ./blueprint-compiler/default.nix { });
 }
