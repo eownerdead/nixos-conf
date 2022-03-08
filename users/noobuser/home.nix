d942c8c66901f778be82d53101ecb6d54772f913
Your Name <you@example.com>
2022-03-08 22:59:25 +0900

    Add some gnome extensions

--- users/noobuser/home.nix
+++ users/noobuser/home.nix
@@ -1,5 +1,4 @@
-{ config, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "noobuser";
@@ -33,13 +32,14 @@
     clippy
     rust-analyzer
     crate2nix
-    gnomeExtensions.dash-to-dock
     firefox-wayland
     thunderbird-wayland
     drawing
     chromium
     pkgconfig
-    clang
+    # error: collision between `/nix/store/xxx-gcc-wrapper-10.3.0/bin/ld' and
+    # `/nix/store/xxx-clang-wrapper-11.1.0/bin/ld'
+    # clang
     gcc
     git
     nixpkgs-fmt
@@ -55,7 +55,45 @@
     gitg
     my.gittyup
     my.blueprint-compiler
-  ];
+  ] ++ (with pkgs.gnomeExtensions; [
+    dash-to-dock
+    fuzzy-app-search
+    alphabetical-app-grid
+  ]);
+
+  dconf.settings = {
+    "org/gnome/desktop/input-sources" = {
+      sources = [
+        (lib.hm.gvariant.mkTuple [ "ibus" "mozc-jp" ])
+        (lib.hm.gvariant.mkTuple [ "xkb" "jp" ])
+      ];
+    };
+
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+        "gnome-fuzzy-app-search@gnome-shell-extensions.Czarlie.gitlab.com"
+        "AlphabeticalAppGrid@stuarthayhurst"
+      ];
+    };
+
+    # Ubuntu-like dock
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      dock-position = "LEFT";
+      dock-fixed = true;
+      custom-theme-shrink = true;
+      extend-height = true;
+    };
+
+    "org/gnome/desktop/wm/preferences" = {
+      button-layout = ":minimize,maximize,close";
+    };
+
+    "org/gnome/desktop/interface" = {
+      clock-show-weekday = true;
+      gtk-theme = "adw-gtk3";
+    };
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
