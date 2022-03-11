2af621e9139a20b7ddbc1034e812516b12f4f1ea
Your Name <you@example.com>
2022-03-11 18:17:51 +0900

    Enable automatic gc

--- system/configuration.nix
+++ system/configuration.nix
@@ -2,10 +2,15 @@
 {
   imports = [ ./hardware-configuration.nix ];
 
-  # Make ready for nix flakes
   nix = {
+    # Make ready for nix flakes
     package = pkgs.nixFlakes;
     extraOptions = ''experimental-features = nix-command flakes'';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 
   boot = {
