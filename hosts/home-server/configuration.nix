453fa5bfa9adfd382d171ef772ca32e4c6f2d5cd
Your Name <you@example.com>
2022-04-04 23:23:03 +0900

    Add nextcloud service

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -24,6 +24,7 @@
     hostName = "home-server";
     useDHCP = false;
     interfaces.enp2s0.useDHCP = true;
+    firewall.allowedTCPPorts = [ 80 ]; # nextcloud
   };
 
   i18n.defaultLocale = "ja_JP.UTF-8";
@@ -43,10 +44,23 @@
 
   programs.git.enable = true;
 
-  services.openssh = {
-    enable = true;
-    passwordAuthentication = false;
-    permitRootLogin = "no";
+  services = {
+    openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      permitRootLogin = "no";
+    };
+
+    # https://discourse.nixos.org/t/minimal-working-nextcloud-config/9316
+    nextcloud = {
+      enable = true;
+      hostName = "10.50.0.2";
+      config = {
+        extraTrustedDomains = [ "192.168.*" ];
+        dbpassFile = "/etc/nextcloud_dbpass.txt";
+        adminpassFile = "/etc/nextcloud_adminpass.txt";
+      };
+    };
   };
 
   system.stateVersion = "21.11";
