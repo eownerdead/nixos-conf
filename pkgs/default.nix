15d03121f7670093cdf009241c4c410806263267
EOWNERDEAD <eownerdead@disroot.org>
2022-06-11 03:36:16 +0900

    Use nixpkgs' blueprint-compiler

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,7 +2,6 @@
 with pkgs; {
   gittyup = (callPackage ./gittyup/default.nix { });
   adw-gtk3 = (callPackage ./adw-gtk3/default.nix { });
-  blueprint-compiler = (callPackage ./blueprint-compiler/default.nix { });
   system-monitoring-center =
     (callPackage ./system-monitoring-center/default.nix { });
   app-icon-preview = (callPackage ./app-icon-preview/default.nix { });
