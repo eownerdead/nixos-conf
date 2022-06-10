dfee1467e16b91117c9512f525404228a49c029c
EOWNERDEAD <eownerdead@disroot.org>
2022-06-11 02:09:25 +0900

    Add firefox profile to noobuser

--- users/noobuser/home.nix
+++ users/noobuser/home.nix
@@ -117,6 +117,7 @@
     firefox = {
       enable = true;
       package = pkgs.firefox-wayland;
+      profiles.default = {};
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
         ublock-origin
         localcdn
