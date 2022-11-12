454129f360b7cdf240df5a5ff7599b99effbdf5c
EOWNERDEAD <eownerdead@disroot.org>
2022-11-12 19:01:08 +0900

    translate-locally-bin: Update

--- pkgs/translate-locally-bin/default.nix
+++ pkgs/translate-locally-bin/default.nix
@@ -1,11 +1,11 @@
 { lib, stdenv, dpkg, libsForQt5, fetchurl, libarchive, qt5 }:
 stdenv.mkDerivation rec {
   name = "translate-locally-bin";
-  version = "0.0.2+b8ec374";
+  version = "0.0.2+136745e";
 
   src = fetchurl {
     url = "https://github.com/XapaJIaMnu/translateLocally/releases/download/latest/translateLocally-v${version}-Ubuntu-20.04.AVX.deb";
-    sha256 = "sha256-+s9hSmFYBs+DsBNvgmV53DEQDWCtG/4bU3sJ76NQoXc=";
+    sha256 = "sha256-8UJyQKsEnvzGnoLuJp0hYnQ0Z1oNvgH+MMI4/z0N6+8=";
   };
 
   nativeBuildInputs = [
