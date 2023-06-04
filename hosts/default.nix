a6d2db9f27c5e62173aa19076e3703f806d4c6e9
EOWNERDEAD <eownerdead@disroot.org>
2023-06-04 16:35:08 +0900

    Add hosts/home-tv

--- hosts/default.nix
+++ hosts/default.nix
@@ -19,6 +19,26 @@
         ];
       });
 
+    home-tv = withSystem "x86_64-linux" ({ pkgs, system, ... }:
+      inputs.nixpkgs.lib.nixosSystem rec {
+        inherit system;
+        specialArgs = { inherit pkgs inputs; };
+        modules = [
+          inputs.disko.nixosModules.disko
+          inputs.home-manager.nixosModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              extraSpecialArgs = { inherit inputs; };
+              users.noobuser = import (../. + "/users/noobuser@home-tv");
+            };
+          }
+          ./home-tv/configuration.nix
+        ];
+      });
+
+
     home-server = withSystem "x86_64-linux" ({ pkgs, system, ... }:
       inputs.nixpkgs.lib.nixosSystem rec {
         system = "x86_64-linux";
