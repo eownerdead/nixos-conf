d76a226f49762b6fb26d092dd0b94080e0eb7093
Your Name <you@example.com>
2023-06-18 00:48:44 +0900

    Update to nixos-23.05

--- users/noobuser/firefox/default.nix
+++ users/noobuser/firefox/default.nix
@@ -17,6 +17,11 @@ in
     enable = true;
     package = pkgs.firefox-wayland;
     profiles.default = {
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        ublock-origin
+        violentmonkey
+        libredirect
+      ];
       userChrome = ''
         @import "firefox-gnome-theme/userChrome.css";
       '';
@@ -33,12 +38,6 @@ in
       extraConfig =
         arkenfox + gnome-theme-userjs + builtins.readFile ./user-overrides.js;
     };
-    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      ublock-origin
-      localcdn
-      violentmonkey
-      libredirect
-    ];
   };
 
   home.file."firefox-gnome-theme" = {
