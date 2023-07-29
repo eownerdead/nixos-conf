8f60082d91ca32569156da9c6ca8d736f19494d5
EOWNERDEAD <eownerdead@disroot.org>
2023-07-30 00:37:20 +0900

    home-server: Update renamed configuration

--- hosts/home-server/configuration.nix
+++ hosts/home-server/configuration.nix
@@ -61,8 +61,10 @@
   services = {
     openssh = {
       enable = true;
-      passwordAuthentication = false;
-      permitRootLogin = "no";
+      settings = {
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
     };
 
     avahi = {
