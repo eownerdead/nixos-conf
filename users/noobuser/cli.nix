d5c26b939b0180c10e6d0c92fd47b1341ccbc817
EOWNERDEAD <eownerdead@disroot.org>
2023-07-29 22:54:34 +0900

    noobuser: Enhnce bash configuration

--- users/noobuser/cli.nix
+++ users/noobuser/cli.nix
@@ -13,6 +13,23 @@
         fortune
         echo
       '';
+      historyControl = [ "erasedups" "ignoredups" "ignorespace" ];
+    };
+    readline = {
+      enable = true;
+      bindings = {
+        "TAB" = "menu-complete";
+        "\\C-h" = "backword-delete-char";
+        "\\M-h" = "backword-kill-word";
+        "\\e[A" = "history-search-backward";
+        "\\e[B" = "history-search-forward";
+      };
+      variables = {
+        colored-stats = true;
+        completion-ignore-case = true;
+        show-all-if-ambiguous = true;
+        skip-completed-text = true;
+      };
     };
     bat.enable = true;
     exa = {
