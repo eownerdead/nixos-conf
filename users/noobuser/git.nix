9c4d6bc7ca648689308482c74d410d0bce19a60d
Your Name <you@example.com>
2023-08-10 00:26:14 +0900

    noobuser: Use git-alias

--- users/noobuser/git.nix
+++ users/noobuser/git.nix
@@ -1,26 +1,23 @@
 { pkgs, ... }:
+let
+  gitAlias =  pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/GitAlias/gitalias/ed036c1fd16c8e690329c594bc028f58c6e3b349/gitalias.txt";
+    hash = "sha256-tcZNjDClFz6Auj+cdWVQxXL5zg+fvIbaz02C/acbBs4=";
+  };
+in
 {
   programs.git = {
     enable = true;
     delta.enable = true;
     userEmail = "you@example.com";
     userName = "Your Name";
-    aliases = {
-      oneline = "log --oneline";
-      last = "log -1 HEAD";
-      unstage = "reset HEAD";
-      shallow = "clone --depth 1";
-      alias = "!git config --get-regexp ^alias.";
-      c = "commit -v";
-      ca = "commit -av";
-      co = "checkout";
-      br = "branch";
-      st = "status";
-      ll = "log --oneline";
+    extraConfig = {
+      include.path = "${gitAlias}";
+      commit.verbose = true;
+      credential.helper = [
+        "cache"
+        "${pkgs.unstable.git-credential-oauth}/bin/git-credential-oauth"
+      ];
     };
-    extraConfig.credential.helper = [
-      "cache"
-      "${pkgs.unstable.git-credential-oauth}/bin/git-credential-oauth"
-    ];
   };
 }
