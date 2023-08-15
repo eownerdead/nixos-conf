138ead60f0982ea8c35586ccfa330cb885f5b4b9
EOWNERDEAD <eownerdead@disroot.org>
2023-08-15 20:08:17 +0900

    noobuser: Add my email address

---
+++ users/noobuser/email.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  accounts.email.accounts."eownerdead@disroot.org" = {
+    address = "eownerdead@disroot.org";
+    userName = "eownerdead@disroot.org";
+    realName = "EOWNERDEAD";
+    primary = true;
+    imap.host = "disroot.org";
+    smtp.host = "disroot.org";
+    gpg = {
+      encryptByDefault = true;
+      signByDefault = true;
+      key = "009E56305CA54D63";
+    };
+  };
+}
+
--- users/noobuser/git.nix
+++ users/noobuser/git.nix
@@ -1,16 +1,25 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 let
   gitAlias = pkgs.fetchurl {
     url = "https://raw.githubusercontent.com/GitAlias/gitalias/ed036c1fd16c8e690329c594bc028f58c6e3b349/gitalias.txt";
     hash = "sha256-tcZNjDClFz6Auj+cdWVQxXL5zg+fvIbaz02C/acbBs4=";
   };
+  email = config.accounts.email.accounts."eownerdead@disroot.org";
 in
 {
+  imports = [
+    ./email.nix
+  ];
+
   programs.git = {
     enable = true;
     delta.enable = true;
-    userEmail = "you@example.com";
-    userName = "Your Name";
+    userEmail = email.address;
+    userName = email.realName;
+    signing = {
+      key = email.gpg.key;
+      signByDefault = true;
+    };
     extraConfig = {
       include.path = "${gitAlias}";
       commit.verbose = true;
@@ -21,3 +30,4 @@ in
     };
   };
 }
+
