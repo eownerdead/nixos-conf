3367af78f206f185b62618d7e4b0e78d0be68a03
EOWNERDEAD <eownerdead@disroot.org>
2024-01-20 23:30:14 +0900

    Remove rest of home-server

--- hosts/default.nix
+++ hosts/default.nix
@@ -39,27 +39,6 @@
           ./home-tv/configuration.nix
         ];
       });
-
-    home-server = withSystem "x86_64-linux" ({ pkgs, system, ... }:
-      inputs.nixpkgs.lib.nixosSystem rec {
-        system = "x86_64-linux";
-        specialArgs = { inherit pkgs; };
-        modules = [
-          inputs.sops-nix.nixosModules.sops
-          inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; };
-              users = {
-                noobuser = import (../. + "/users/noobuser@home-server");
-              };
-            };
-          }
-          ./home-server/configuration.nix
-        ];
-      });
   };
 }
 
